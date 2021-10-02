ada795f392cd41fd4748a3958c791f3b366852fc
hinha <martinuz.dawan9@gmail.com>
2021-10-02 14:22:32 +0700

    fix typo

--- backend/domain/user.go
+++ backend/domain/user.go
@@ -1,14 +1,19 @@
 package domain
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
 	gorm.Model
 	ID        string     `json:"id" gorm:"type:varchar(50);primaryKey"`
 	FirstName string     `json:"firstName" gorm:"type:varchar(50);not null"`
-	LastName  string     `json:"LastName" gorm:"type:varchar(50);not null"`
+	LastName  string     `json:"lastName" gorm:"type:varchar(50);not null"`
 	Email     string     `json:"email" gorm:"type:text;not null"`
 	Password  string     `json:"password" gorm:"type:text;not null"`
 	Status    bool       `json:"status" gorm:"default:false"`
+	EmailVerified bool `json:"emailVerified" gorm:"default:false"`
+	EmailCreateVerified time.Time `json:"-"`
 	Notebook  []Notebook `gorm:"foreignKey:UserID"`
 }
